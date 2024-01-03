a6d2ebe3e6061cb1c896907233536e85e2474c88
Aleksei <jermlay2010@gmail.com>
2024-01-03 21:06:09 +0300

    added some functions and stuff

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); 
+    }
+}
+start();
 
 
 const personalMovieDB  = {
@@ -8,20 +17,61 @@ const personalMovieDB  = {
     movies: {},
     actors: {},
     genres: [],
-    private: false,
+    privat: false,
 };
 
 
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+       personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+    
+}
+writeYourGenres();
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('Error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов')
+        // alert('Просмотрено довольно мало фильмов')
+    } else if (personalMovieDB.count < 30) {
+        console.log('Вы классический зритель')
+        // alert('Вы классический зритель')
+    } else {
+        console.log('Вы киноман')
+        // alert('Вы киноман')
+    }   
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
 
 
